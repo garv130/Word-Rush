fa139093d68a5579652490c81288d2db5bdd39ff
garv130 <13garvsharma@gmail.com>
2025-07-29 22:58:39 -0400

    Update wordrush.js

--- wordrush.js
+++ wordrush.js
@@ -2,3 +2,10 @@
 let background;
 let backgroundWidth = 640;
 let backgoundHeight = 640;
+let context;
+
+window.onload = function(){
+    document.getElementById("background");
+    background.height = backgoundHeight;
+    background.width = backgroundWidth;
+}
