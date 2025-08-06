67a0365397db4be9a708dd99d57dd04d10bc216a
garv130 <13garvsharma@gmail.com>
2025-08-06 19:48:00 -0400

    Update wordrush.js

--- wordrush.js
+++ wordrush.js
@@ -17,6 +17,35 @@ let character = {
     height: charHeight,
 }
 
+let words = [];
+let currentInput = "";
+let wordSpeed = 2;
+
+//words
+class Word{
+    constructor(text, x, y, speed){
+        this.text = text;
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.width = context.measureText(text).width;
+    }
+
+
+    draw(){
+        context.font = "24px Courier New";
+        context.fillStyle = "white";
+        context.fillText(this.text, this.x, this.y);
+    }
+
+    update(){
+        this.x -= this.speed;
+        this.draw();
+    }
+
+}
+
+
 window.onload = function(){
     background = document.getElementById("background");
     background.height = backgroundHeight;
@@ -31,4 +60,15 @@ window.onload = function(){
     charImg.onload = function(){
     context.drawImage(charImg, character.x, character.y, character.width, character.height);
     }
+
+    requestAnimationFrame(update);
+}
+
+function update(){
+    requestAnimationFrame(update);
+    context.clearRect(0,0,background.width,background.height)
+
+
+    context.drawImage(charImg, character.x, character.y, character.width, character.height);
+
 }
