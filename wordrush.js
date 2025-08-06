351e24547768f3ea77709e8d771858669c208f73
garv130 <13garvsharma@gmail.com>
2025-08-06 15:03:59 -0400

    Update wordrush.js

--- wordrush.js
+++ wordrush.js
@@ -4,8 +4,22 @@ let backgroundWidth = 640;
 let backgoundHeight = 640;
 let context;
 
+//character
+let charWidth = 30;
+let charHeight = 50;
+let charX = backgroundWidth/4;
+let charY = backgroundHeight/2;
+
+let character = {
+    x : charX,
+    y : charY,
+    width: charWidth,
+    height: charHeight,
+}
+
 window.onload = function(){
-    document.getElementById("background");
+    background = document.getElementById("background");
     background.height = backgoundHeight;
     background.width = backgroundWidth;
+    context = background.getContext("2d") //This is for drawing on the background
 }
